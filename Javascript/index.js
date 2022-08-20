c706f80ca525991e4e37b53c598e768e53596a32
GUIHFELIX <guifelix339@gmail.com>
2022-08-20 20:49:27 -0300

    Encontro Presencial 3

---
+++ Javascript/index.js
@@ -0,0 +1,11 @@
+
+    function darkMode() {
+        var element = document.body;
+     element.classList.toggle("dark-mode");
+  }
+
+  function mudarFonte(){
+
+    document.body.classList.toggle("mudarFonte");
+    
+}
